20a5d9a6fa33fc6fd2cf7da7fb6603e7af8e199e
Filip Wicha <5021567@gmail.com>
2017-11-25 11:47:37 +0100

    changes in packet.cs, added comments

--- ProjectTS/Packet.cs
+++ ProjectTS/Packet.cs
@@ -22,10 +22,10 @@ namespace ProjectTS
         BitArray bitArr;
         int index = 0;
         int size;
-        
-        BitArray operation = new BitArray(3);
-        int number1;
-        int number2;
+                                              
+        BitArray operation = new BitArray(3); //dla dwuargumentowych dodatkowe operacje (x -pierwszy argument, y -drugi argument):
+        int number1;                          //4: x^y; 5: pierwiasek stopnia y z x; 6: log o podstawie y z x; 7 czy x równa się y.
+        int number2;                          //dla wieloargumentowych tylko operacje +-*/
         BitArray status = new BitArray(2);
         BitArray id = new BitArray(8);
         BitArray state = new BitArray(2); //odpowiada za informowanie serwera czy jest to pakiet           \
@@ -33,6 +33,112 @@ namespace ProjectTS
                                           //"00"-operacja dwuargumentowa,                                  |-czekam na odpowiedz Marka
                                           //"01"-operacja wieloargumentowa, ale nie ostatni pakiet,        | czy tak może być to zrobione
                                           //"10"-operacja wieloargumentowa, ostatni pakiet.                / 
+
+        void SetOperation(int num) //ustawianie operacji (3bit)
+        {
+            switch (num)
+            {
+                case 0: //dodawanie
+                    operation.Set(0, false);
+                    operation.Set(1, false);
+                    operation.Set(2, false);
+                    break;
+
+                case 1: //odejmowanie
+                    operation.Set(0, false);
+                    operation.Set(1, false);
+                    operation.Set(2, true);
+                    break;
+
+                case 2: //mnożenie
+                    operation.Set(0, false);
+                    operation.Set(1, true);
+                    operation.Set(2, false);
+                    break;
+
+                case 3: //dzielenie
+                    operation.Set(0, false);
+                    operation.Set(1, true);
+                    operation.Set(2, true);
+                    break;
+
+                case 4: //potega
+                    operation.Set(0, true);
+                    operation.Set(1, false);
+                    operation.Set(2, false);
+                    break;
+
+                case 5: //pierwiastek
+                    operation.Set(0, true);
+                    operation.Set(1, false);
+                    operation.Set(2, true);
+                    break;
+
+                case 6: //logarytm
+                    operation.Set(0, true);
+                    operation.Set(1, true);
+                    operation.Set(2, false);
+                    break;
+
+                case 7: //czy równa
+                    operation.Set(0, true);
+                    operation.Set(1, true);
+                    operation.Set(2, true);
+                    break;
+            }
+        }
+
+        void SetState(int num)
+        {
+            switch (num)
+            {
+                case 0: //operacja 2 argumentowa
+                    state.Set(0, false);
+                    state.Set(1, false);
+                    break;
+
+                case 1: //operacja wieloargumentowa, ale nie ostatni pakiet
+                    state.Set(0, false);
+                    state.Set(1, true);
+                    break;
+
+                case 2: //operacja wieloargumentowa, ostatni pakiet
+                    state.Set(0, true);
+                    state.Set(1, false);
+                    break;
+
+                case 3: //nie zdefiniowane
+                    state.Set(0, true);
+                    state.Set(1, true);
+                    break;
+            }
+        } //ustawianie state (czy operacja dwu czy wieloargumentowa) (2bit)
+
+        void SetStatus(int num)
+        {
+            switch (num)
+            {
+                case 0: //status 0
+                    status.Set(0, false);
+                    status.Set(1, false);
+                    break;
+
+                case 1: //status 1
+                    status.Set(0, false);
+                    status.Set(1, true);
+                    break;
+
+                case 2: //status 2
+                    status.Set(0, true);
+                    status.Set(1, false);
+                    break;
+
+                case 3: //status 3
+                    status.Set(0, true);
+                    status.Set(1, true);
+                    break;
+            }
+        } //ustawianie statusu (2bit)
 
         public Packet(int size)
         {
